01ba260fc9c62898d8b746c45d5cd325612e1f66
andreacristancho <andreacristancho01@gmail.com>
2024-09-18 17:25:23 -0500

    My course page_before filters and large view

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
+  
+
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = currentYear;
+  
+  
   // Last modified information
+
   const lastModified = document.getElementById("lastModified");
   const lastModifiedDate = document.lastModified;
-  lastModified.textContent = `Last Modified: ${lastModifiedDate}`;
+  lastModified.textContent = `Last Updated: ${lastModifiedDate}`;
+});
+
+
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
 });
