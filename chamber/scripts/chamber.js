fcd819d88161c3d8021bc1815951bbeddfc7d8dd
andreacristancho <andreacristancho01@gmail.com>
2024-09-24 19:40:01 -0500

    Chamber with menu hamb ugly

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    const currentYear = new Date().getFullYear();
+    
+    document.getElementById("currentyear").textContent = currentYear;
+      
+      
+      // Last modified information
+    
+      const lastModified = document.getElementById("lastModified");
+      const lastModifiedDate = document.lastModified;
+      lastModified.textContent = `Last Updated: ${lastModifiedDate}`;
+    });
+    
+    
+    
+    const hamButton = document.querySelector("#menu");
+    const navigation = document.querySelector("nav");
+    
+    hamButton.addEventListener("click", () => {
+      navigation.classList.toggle("open");
+      hamButton.classList.toggle("open");
+    });
+    
