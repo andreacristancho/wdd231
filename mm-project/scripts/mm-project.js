5df3ea2b8a1a9953b90f568b7f78f5911a2ca543
andreacristancho <andreacristancho01@gmail.com>
2024-10-19 22:02:03 -0500

    Project beggining

---
+++ mm-project/scripts/mm-project.js
@@ -0,0 +1,94 @@
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
+  
+    // Última fecha de modificación
+    const lastModified = document.getElementById("lastModified");
+    const lastModifiedDate = document.lastModified;
+    lastModified.textContent = `Last Updated: ${lastModifiedDate}`;
+  });
+
+  const url = 'data/news.json';
+  
+  // Función para obtener los datos JSON
+  const getNews = async () => {
+    try {
+      const news = await jsonFetch(url);
+      
+      console.log(news);
+      displayNews(news);
+    } catch (error) {
+      console.error('Error fetching the news', error);
+    }
+  };
+
+   // Función para hacer fetch del archivo JSON
+   async function jsonFetch(url) {
+    try {
+      const response = await fetch(url);
+      console.log(response); // Para verificar el estado de la respuesta
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json(); // Procesa la respuesta JSON
+      console.log('Datos JSON recibidos:', data); // OJO PARA VER SI DESCARGA LA INFO
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return null;
+    }
+   }
+
+  // Función para mostrar los miembros en la página
+  const displayNews = (news) => {
+    
+    const cards = document.querySelector("div.cards");
+    cards.innerHTML = ""; // Limpiar el contenido previo
+  
+
+    news.forEach((newsItem) => {
+        let card = document.createElement("section");
+        card.classList.add("article");
+    
+        // Título del miembro
+        let h2 = document.createElement("h2");
+        h2.innerHTML = `<span class="titleNew"> ${newsItem.title}</span>`;
+    
+        let h3 = document.createElement("h3");
+        h3.innerHTML = `<span class="dateNew"> ${newsItem.date}</span>`;
+    
+        let p = document.createElement("p");
+        p.innerHTML = `${newsItem.news}`;
+    
+        let imgNews = document.createElement("div");
+        imgNews.classList.add("imgNews");
+
+           // Imagen del miembro
+      let portrait = document.createElement("img");
+      portrait.setAttribute("src", newsItem.image);
+      portrait.setAttribute("alt", newsItem.date);
+      portrait.setAttribute("loading", "lazy");
+      portrait.setAttribute("width", "85");
+      portrait.setAttribute("height", "110");
+
+  
+      // Agregar título, estadísticas y la imagen a la tarjeta
+      card.appendChild(h2);
+      card.appendChild(h3);
+      card.appendChild(p);
+      imgNews.appendChild(portrait);
+      card.appendChild(imgNews);
+  
+      // Añadir la tarjeta al contenedor de cards
+      cards.appendChild(card);
+    });
+  };
